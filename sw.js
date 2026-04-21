00928c9e2bb6b1042cf3115597e3e5f659e662fa
kantarokotasan-a11y <kantaro.kotasan@gmail.com>
2026-04-22 08:29:39 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE='inv-app-v1';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES).catch(()=>{})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+      if(resp.ok&&e.request.method==='GET'){
+        const clone=resp.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,clone));
+      }
+      return resp;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
