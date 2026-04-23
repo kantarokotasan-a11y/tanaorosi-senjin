cdff331fe5e6776514933c16a6767eb3c0ae7bec
kantarokotasan-a11y <kantaro.kotasan@gmail.com>
2026-04-23 12:52:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='inv-app-v10';
+const CACHE='inv-app-v11';
 const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
